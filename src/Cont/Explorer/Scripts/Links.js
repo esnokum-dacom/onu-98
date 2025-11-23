61478bf3652bdd350e343bd44d863ac4f9029e41
esnokum-dacom <esnokaskas@gmail.com>
2025-11-22 18:27:12 -0600

    finals changes (for now)

--- src/Cont/Explorer/Scripts/Links.js
+++ src/Cont/Explorer/Scripts/Links.js
@@ -2,7 +2,9 @@ import initRendersButton from "../../ScriptsAll/Window.js";
 
 const Renders = document.getElementById("CreateCW");
 
-initRendersButton("CreateCW", "Renders", "/src/Props/Icons/MIE.png", "/src/Cont/Renders/index.html", 600, 500);
-initRendersButton("CreateBl", "Log media ", "/src/Props/Icons/MIE.png", "/src/Cont/Blog/index.html", 600, 500);
-initRendersButton("CreateTS", "Tastes", "/src/Props/Icons/MIE.png", "/src/Cont/MyTastes/index.html", 600, 350);
+"../../../Props/Icons/MIE.png"
+
+initRendersButton("CreateCW", "Renders", "/../../../Props/Icons/MIE.png", "/src/Cont/Renders/index.html", 600, 500);
+initRendersButton("CreateBl", "Log media ", "/../../../Props/Icons/MIE.png", "/src/Cont/Blog/index.html", 600, 500);
+initRendersButton("CreateTS", "Tastes", "/../../../Props/Icons/MIE.png", "/src/Cont/MyTastes/index.html", 600, 350);
 
