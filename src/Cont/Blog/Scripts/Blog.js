05b38ded10ae9f3929f07c7e45c36d960f028d64
esnokum-dacom <esnokaskas@gmail.com>
2025-11-27 21:30:53 -0600

    Changes for the Log Media and his interface

--- src/Cont/Blog/Scripts/Blog.js
+++ src/Cont/Blog/Scripts/Blog.js
@@ -9,11 +9,23 @@ async function GetMediaLog() {
     const data = await response.json();
 
     data.allmedia
+
+    const Music = data.allmedia.filter(item => item.type === "Music")
+    const Anime = data.allmedia.filter(item => item.type === "Animé")
+    const Serie = data.allmedia.filter(item => item.type === "Serie")
+     
     data.allmedia.sort((a, b) => b.id - a.id)
-      .forEach(media => {
+      Music.forEach(media => {
+        const ContainerMusic = document.getElementById("ContainerMusic")
+        ContainerImgs.append(ContainerMusic)
+
+        const InterContainer = document.createElement("div")
+        InterContainer.className = "PostMedia"
+        ContainerMusic.append(InterContainer)
+
         const RContainer = document.createElement("div")
         RContainer.className = "CdocAb"
-        ContainerImgs.append(RContainer)
+        InterContainer.append(RContainer)
 
         const ImageTitle = document.createElement("h2")
         ImageTitle.innerText = media.medianame;
@@ -55,14 +67,163 @@ async function GetMediaLog() {
         date.innerText = media.date
         ContainerRight.appendChild(date)
 
+        const containerTH = document.createElement("div")
+        containerTH.className = "CdocAb"
+        InterContainer.appendChild(containerTH)
+
+        const IdTh = document.createElement("h2")
+        IdTh.innerText = "Thought"
+        IdTh.className = "Bander"
+        IdTh.id = "TextBander"
+        IdTh.style.textAlign = "center"
+        containerTH.appendChild(IdTh)
+
         const thought = document.createElement("p")
         thought.className = "Thth"
         thought.innerText = media.thought
-        ContainerRight.appendChild(thought)
+        containerTH.appendChild(thought)
+      });
+
+      Anime.forEach(media => {
+        const ContainerAnimes = document.getElementById("ContainerAnimes")
+        ContainerImgs.append(ContainerMusic)
+
+        const InterContainer = document.createElement("div")
+        InterContainer.className = "PostMedia"
+        ContainerAnimes.append(InterContainer)
+
+        const RContainer = document.createElement("div")
+        RContainer.className = "CdocAb"
+        InterContainer.append(RContainer)
+
+        const ImageTitle = document.createElement("h2")
+        ImageTitle.innerText = media.medianame;
+        ImageTitle.className = "Bander"
+        ImageTitle.id = "TextBander"
+        RContainer.appendChild(ImageTitle)
+
+        const containerAbsolute = document.createElement("div")
+        containerAbsolute.className = "eltodopoderoso"
+        RContainer.appendChild(containerAbsolute)
+
+        const ContainerLeft = document.createElement("div")
+        ContainerLeft.className = "ContLeft"
+        containerAbsolute.appendChild(ContainerLeft)
+
+        const image = document.createElement('img');
+        image.src = media.imageport;
+        image.className = "Render-img"
+        ContainerLeft.appendChild(image);
+
+        const ContainerRight = document.createElement("div")
+        ContainerRight.className = "ContRig"
+        containerAbsolute.appendChild(ContainerRight)
+
+        const score = document.createElement("h2")
+        score.innerText = media.score
+        ContainerRight.appendChild(score)
+
+        const finished = document.createElement("p")
+        if (media.finished) {
+          finished.innerText = "Finished *"
+        }
+        else {
+          finished.innerText = "In progress -"
+        }
+        ContainerRight.appendChild(finished)
+
+        const date = document.createElement("p")
+        date.innerText = media.date
+        ContainerRight.appendChild(date)
+
+        const containerTH = document.createElement("div")
+        containerTH.className = "CdocAb"
+        InterContainer.appendChild(containerTH)
+
+        const IdTh = document.createElement("h2")
+        IdTh.innerText = "Thought"
+        IdTh.className = "Bander"
+        IdTh.id = "TextBander"
+        IdTh.style.textAlign = "center"
+        containerTH.appendChild(IdTh)
+
+        const thought = document.createElement("p")
+        thought.className = "Thth"
+        thought.innerText = media.thought
+        containerTH.appendChild(thought)
+      });
+
+      Serie.forEach(media => {
+        const ContainerSerie = document.getElementById("ContainerSeries")
+        ContainerImgs.append(ContainerMusic)
+
+        const InterContainer = document.createElement("div")
+        InterContainer.className = "PostMedia"
+        ContainerSerie.append(InterContainer)
+
+        const RContainer = document.createElement("div")
+        RContainer.className = "CdocAb"
+        InterContainer.append(RContainer)
+
+        const ImageTitle = document.createElement("h2")
+        ImageTitle.innerText = media.medianame;
+        ImageTitle.className = "Bander"
+        ImageTitle.id = "TextBander"
+        RContainer.appendChild(ImageTitle)
+
+        const containerAbsolute = document.createElement("div")
+        containerAbsolute.className = "eltodopoderoso"
+        RContainer.appendChild(containerAbsolute)
+
+        const ContainerLeft = document.createElement("div")
+        ContainerLeft.className = "ContLeft"
+        containerAbsolute.appendChild(ContainerLeft)
+
+        const image = document.createElement('img');
+        image.src = media.imageport;
+        image.className = "Render-img"
+        ContainerLeft.appendChild(image);
+
+        const ContainerRight = document.createElement("div")
+        ContainerRight.className = "ContRig"
+        containerAbsolute.appendChild(ContainerRight)
+
+        const score = document.createElement("h2")
+        score.innerText = media.score
+        ContainerRight.appendChild(score)
+
+        const finished = document.createElement("p")
+        if (media.finished) {
+          finished.innerText = "Finished *"
+        }
+        else {
+          finished.innerText = "In progress -"
+        }
+        ContainerRight.appendChild(finished)
+
+        const date = document.createElement("p")
+        date.innerText = media.date
+        ContainerRight.appendChild(date)
+
+        const containerTH = document.createElement("div")
+        containerTH.className = "CdocAb"
+        InterContainer.appendChild(containerTH)
+
+        const IdTh = document.createElement("h2")
+        IdTh.innerText = "Thought"
+        IdTh.className = "Bander"
+        IdTh.id = "TextBander"
+        IdTh.style.textAlign = "center"
+        containerTH.appendChild(IdTh)
+
+        const thought = document.createElement("p")
+        thought.className = "Thth"
+        thought.innerText = media.thought
+        containerTH.appendChild(thought)
       });
 
   } catch (error) {
-
+    console.error(error)
   }
 }
 
