6f7cb10ce62228e2172324fe277eee83f7bdab56
esnokum-dacom <esnokaskas@gmail.com>
2025-12-27 21:50:33 -0600

    Solve icons issue (again)

--- src/Cont/MyTastes/Buttons/Nav.js
+++ src/Cont/MyTastes/Buttons/Nav.js
@@ -4,19 +4,19 @@ if (window.parent.XpWindows) {
   initRendersButton("CreateCW", "Music", "/../../../Props/Icons/MIE.png", "/src/Cont/MyTastes/Topics/Music/index.html", 700, 500);
 }
 else {
-  initRendersButton("CreateCW", "Music", "/../../../Props/Icons/wxp/iEX.ico", "/src/Cont/MyTastes/Topics/Music/index.html", 700, 500, { windowOn: "NXPWindow", windowOff: "NXPWindowOff", windowInac: "NXPWindowINC", tBarOn: "NXPBarOn", tBarOff: "NXPBarOff", webOn: "webXPOs", ButtonM: "ButtonX", ButtonX: "ButtonC", ButImgX: "IconXP", ButImgM: "IconMP", appTab: "TbWinXp", windowMax: "NXPWindowMAX" });
+  initRendersButton("CreateCW", "Music", "/../../../Props/Icons/wxp/IEX.ico", "/src/Cont/MyTastes/Topics/Music/index.html", 700, 500, { windowOn: "NXPWindow", windowOff: "NXPWindowOff", windowInac: "NXPWindowINC", tBarOn: "NXPBarOn", tBarOff: "NXPBarOff", webOn: "webXPOs", ButtonM: "ButtonX", ButtonX: "ButtonC", ButImgX: "IconXP", ButImgM: "IconMP", appTab: "TbWinXp", windowMax: "NXPWindowMAX" });
 }
 
 if (window.parent.XpWindows) {
   initRendersButton("CreateBl", "Hobbies", "/../../../Props/Icons/iEX.png", "/src/Cont/MyTastes/Topics/Hobbies/index.html", 800, 500);
 }
 else {
-  initRendersButton("CreateBl", "Hobbies", "/../../../Props/Icons/wxp/iEX.ico", "/src/Cont/MyTastes/Topics/Hobbies/index.html", 800, 500, { windowOn: "NXPWindow", windowOff: "NXPWindowOff", windowInac: "NXPWindowINC", tBarOn: "NXPBarOn", tBarOff: "NXPBarOff", webOn: "webXPOs", ButtonM: "ButtonX", ButtonX: "ButtonC", ButImgX: "IconXP", ButImgM: "IconMP", appTab: "TbWinXp", windowMax: "NXPWindowMAX" });
+  initRendersButton("CreateBl", "Hobbies", "/../../../Props/Icons/wxp/IEX.ico", "/src/Cont/MyTastes/Topics/Hobbies/index.html", 800, 500, { windowOn: "NXPWindow", windowOff: "NXPWindowOff", windowInac: "NXPWindowINC", tBarOn: "NXPBarOn", tBarOff: "NXPBarOff", webOn: "webXPOs", ButtonM: "ButtonX", ButtonX: "ButtonC", ButImgX: "IconXP", ButImgM: "IconMP", appTab: "TbWinXp", windowMax: "NXPWindowMAX" });
 }
 
 if (window.parent.XpWindows) {
   initRendersButton("CreateTS", "Books", "/../../../Props/Icons/MIE.png", "/src/Cont/MyTastes/Topics/Books/index.html", 600, 400);
 }
 else {
-  initRendersButton("CreateTS", "Books", "/../../../Props/Icons/wxp/iEX.ico", "/src/Cont/MyTastes/Topics/Books/index.html", 600, 400, { windowOn: "NXPWindow", windowOff: "NXPWindowOff", windowInac: "NXPWindowINC", tBarOn: "NXPBarOn", tBarOff: "NXPBarOff", webOn: "webXPOs", ButtonM: "ButtonX", ButtonX: "ButtonC", ButImgX: "IconXP", ButImgM: "IconMP", appTab: "TbWinXp", windowMax: "NXPWindowMAX" });
+  initRendersButton("CreateTS", "Books", "/../../../Props/Icons/wxp/IEX.ico", "/src/Cont/MyTastes/Topics/Books/index.html", 600, 400, { windowOn: "NXPWindow", windowOff: "NXPWindowOff", windowInac: "NXPWindowINC", tBarOn: "NXPBarOn", tBarOff: "NXPBarOff", webOn: "webXPOs", ButtonM: "ButtonX", ButtonX: "ButtonC", ButImgX: "IconXP", ButImgM: "IconMP", appTab: "TbWinXp", windowMax: "NXPWindowMAX" });
 }
